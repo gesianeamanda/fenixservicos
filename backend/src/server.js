1845a0d234957f4e92fb9133775a31c88dfed186
Ebraim Carvalho <ebraimcarvalho@gmail.com>
2019-10-03 20:18:43 -0300

    app.use express.json()

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,8 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ message: "Hello World!"});
+// GET(BUSCAR INFO), POST(CRIAR INFO), PUT(ALTERAR INFO), DELETE(DELETAR INFO)
+
+// req.query = Acessasr query params (para filtros)
+// req.params = Acessar route params (para edição e delete)
+// req.body = Acessar corpo da requisição (para criação e edição)
+
+app.use(express.json());
+
+app.post('/users', (req, res) => {
+  return res.json(req.body);
 });
 
 app.listen(3333);
