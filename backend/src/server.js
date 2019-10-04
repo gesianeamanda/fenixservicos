d88ca36d3a3ad32cbd4bd159e86521240b81ffb3
Ebraim Carvalho <ebraimcarvalho@gmail.com>
2019-10-03 21:32:31 -0300

    mongodb e mongoose

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://semana:semana@cluster0-ecaw2.mongodb.net/semana09?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 
 // GET(BUSCAR INFO), POST(CRIAR INFO), PUT(ALTERAR INFO), DELETE(DELETAR INFO)
 
@@ -9,9 +17,6 @@ const app = express();
 // req.body = Acessar corpo da requisição (para criação e edição)
 
 app.use(express.json());
-
-app.post('/users', (req, res) => {
-  return res.json(req.body);
-});
+app.use(routes);
 
 app.listen(3333);
