b09927160f679baf56e0e859cf30fb75f7d32e28
Ebraim Carvalho <ebraimcarvalho@gmail.com>
2019-10-07 13:30:20 -0300

    Mobile criando SpotList

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, AsyncStorage } from 'react-native';
+import { SafeAreaView, StyleSheet, Image, AsyncStorage } from 'react-native';
+
+import SpotList from '../components/SpotList';
+
+import logo from '../assets/logo.png';
 
 export default function List() {
   const [techs, setTechs] = useState([]);
@@ -12,5 +16,24 @@ export default function List() {
     })
   }, []);
 
-  return <Text>{techs}</Text>
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.logo} source={logo} />
+
+      <SpotList />
+    </SafeAreaView>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+},
+
+  logo: {
+    height: 32,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    marginTop: 10,
+  },
+});
