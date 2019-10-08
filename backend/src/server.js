b1d6fd0faa4274465e68a39eede1d847903e4cd7
Ebraim Carvalho <ebraimcarvalho@gmail.com>
2019-10-08 13:40:52 -0300

    ajustes finais socket.io

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,13 +3,33 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http');
+
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect('mongodb+srv://semana:semana@cluster0-ecaw2.mongodb.net/semana09?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+})
+
+const connecetdUsers = {};
+
+io.on('connection', socket => {
+  const { user_id } = socket.handshake.query;
+
+  connecetdUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connecetdUsers = connecetdUsers;
+
+  return next();
 })
 
 // GET(BUSCAR INFO), POST(CRIAR INFO), PUT(ALTERAR INFO), DELETE(DELETAR INFO)
@@ -23,4 +43,4 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
